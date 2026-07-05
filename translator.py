6611916586b1809059cbf5fcec66198bc648b9a0
RypekGrief <recepgul826@gmail.com>
2026-07-05 14:53:49 +0300

    perf: minor performance update

--- translator.py
+++ translator.py
@@ -1,9 +1,13 @@
 from deep_translator import GoogleTranslator
 
+_translator_cache = {}
+
 
 def translate(text, target_language="en"):
     try:
-        result = GoogleTranslator(source="auto", target=target_language).translate(text)
-        return result
+        key = target_language
+        if key not in _translator_cache:
+            _translator_cache[key] = GoogleTranslator(source="auto", target=target_language)
+        return _translator_cache[key].translate(text)
     except Exception:
         return None
